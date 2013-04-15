d21bc86cdb0f4f5c34af8b75c335230576e430d2
easonfafa <mac@rrdhcp-10-32-126-19.redrover.cornell.edu>
2013-04-15 12:15:27 -0400

    new rails application with mongodb

---
+++ biography/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Biography::Application.config.secret_token = '63cb578b4ea9cd19ff64cb0623fe00da4aeef0649f263dc676d1fdfdfeced9c1186a3ffcb494e834bd35ff570562bebe5667a4e79f7dd7059652a71c45b236a8'
